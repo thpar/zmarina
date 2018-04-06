ebd5eb8af04d91c9d6792b2d5955d5855cc93c6c
Thomas Van Parys <thpar@psb.ugent.be>
2018-04-06 12:54:10 +0200

    Fix bad PHP tag

--- plugins/genelist/baskets/listbarang.php
+++ plugins/genelist/baskets/listbarang.php
@@ -49,7 +49,7 @@ if (mysql_num_rows($defaultresults) != 0) {
 
             ?></td>
          <td>
-            <?
+            <?php
                if($data['gene_basket_id']==$default_g_id){
                	?>
             <script>
