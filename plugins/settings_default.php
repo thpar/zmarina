b404cf443666c897a518b889c63e4106246319da
Thomas Van Parys <thpar@psb.ugent.be>
2018-04-10 10:15:29 +0200

    Rename template settings file and add general db configuration.
    Remove global and GLOBALS[] keywords as they have no effect outside of a function.

--- plugins/settings_default.php
+++ plugins/settings_default.php
@@ -2,11 +2,9 @@
 date_default_timezone_set('CET');
 
 /*Here you can listed genie databases that you are going to use for different genomes and their corresponding species names. eg: "database_1" => "species 1",*/
-global $db_species_array;
 $db_species_array=array("database1"=>"species 1","database2"=>"species 2");
 
 /*Here you can listed background colors correspond to each species. eg: "database_1" => "#FF0000",*/
-global $db_species_color_array;
 $db_species_color_array=array("database1"=>"#86c0a6","database2"=>"#83afae");
 
 
@@ -22,14 +20,19 @@ if(isset($_COOKIE['genie_select_species'])) {
 }
 
 /*Please state the mysql username and password for above databases. It's important to grant only SELECT permissio to all the tables except defaultgenebaskets and genebasket tables*/
-global $db_url;
-$db_url=  array ('genelist'=>'mysqli://database_username:database_password@localhost/'.$selected_database);
+$db_settings = array(
+    'host' => "",
+    'user' => "",
+    'pass' => ""
+);
+$db_url = array ('genelist'=>
+                 "mysqli://".$db_settings['user'].":".$db_settings['pass']."@".$db_settings['host']."/".$selected_database);
+
 
 /*Define your base url with trailing slash*/
-$GLOBALS["base_url"]='https://yourdomainname.com';
+$base_url = 'https://yourdomainname.com';
 
 /*You dont need to change anything here*/
-global $uuid;
 if ( isset($_SERVER["REMOTE_ADDR"]) )    {
     $ip = '' . $_SERVER["REMOTE_ADDR"] . '';
 } else if ( isset($_SERVER["HTTP_X_FORWARDED_FOR"]) )    {
