48d57c5f888fbc281439f7f2f4d57ed87a9ff101
Thomas Van Parys <thpar@psb.ugent.be>
2018-04-10 10:17:41 +0200

    Use general db config for genelist

--- plugins/genelist/crud/koneksi.php
+++ plugins/genelist/crud/koneksi.php
@@ -1,7 +1,5 @@
 <?php
 include("../../settings.php");
-global $db_url; 
-$private_url = parse_url($db_url['genelist']);
-mysql_connect($private_url['host'], $private_url['user'], $private_url['pass']) or die(mysql_error());
-mysql_select_db(str_replace('/', '', $private_url['path'])) or die(mysql_error());
+mysql_connect($db_settings['host'], $db_settings['user'], $db_settings['pass']) or die(mysql_error());
+mysql_select_db($selected_database) or die(mysql_error());
 ?>
