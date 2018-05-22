2fdfec3829232cc415e247e94f82069050b228ab
Thomas Van Parys <thpar@psb.ugent.be>
2018-05-22 14:54:11 +0200

    Take into account the REQUEST_URI includes GET parameters that need to be stripped away in our case.

--- plugins/help/index.php
+++ plugins/help/index.php
@@ -1,18 +1,19 @@
 <?php 
-$subdir_arr = explode("/", $_SERVER['REQUEST_URI']);
+$requested_path = explode('?', $_SERVER['REQUEST_URI'])[0];
+$requested_page = strtolower(end(explode('/', $requested_path)));
 $mennu_arr= explode("<br />", $c['menu']);
 $menu_exist=false;
 
 foreach($mennu_arr as $searched){
-	if(trim(strtolower($searched)) == strtolower(end($subdir_arr)) || 
-       trim(strtolower($searched)) == "-".strtolower(end($subdir_arr))){
+	if(trim(strtolower($searched)) == $requested_page || 
+       trim(strtolower($searched)) == "-".$requested_page){
 		$menu_exist=true;
 	}
 }
 
-if(strtolower(basename(dirname(__FILE__))) == strtolower(end($subdir_arr)) && $menu_exist){
+if(strtolower(basename(dirname(__FILE__))) == $requested_page && $menu_exist){
 	$c['initialize_tool_plugin']=true;
-	$c['tool_plugin']=strtolower(end($subdir_arr));
+	$c['tool_plugin']=$requested_page;
 
 }
 ?>
