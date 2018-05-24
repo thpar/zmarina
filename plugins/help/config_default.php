473f96bef1ca86447d703833849085ab7b17c101
Thomas Van Parys <thpar@psb.ugent.be>
2018-05-24 13:14:45 +0200

    Help plugin specific config file

---
+++ plugins/help/config_default.php
@@ -0,0 +1,19 @@
+<?php
+/**
+   Help plugin specific configuration.
+   Make a copy of this file to `config.php` before filling out the values.
+*/
+
+$help_plugin_config = array(
+
+    /**
+       Credentials for help database
+     */
+    'database' => array(
+        'host' => '',
+        'user' => '',
+        'pass' => '',
+        'db' => ''
+    )
+);
+?>
