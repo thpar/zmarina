659882ace598ef52ef93aecdb5fe905c1a940249
Thomas Van Parys <thpar@psb.ugent.be>
2018-05-24 17:52:01 +0200

    Retrieve flanking sequences with blastdbcmd and the plugin config of the gene plugin (fixes #11)

--- plugins/autocomplete/test/retrieveflanksequence.php
+++ plugins/autocomplete/test/retrieveflanksequence.php
@@ -6,50 +6,47 @@ $picea_basic_end=trim($_GET['picea_basic_end']);
 $picea_basic_chromosome=trim($_GET['picea_basic_chromosome']);
 $plus_minus=trim($_GET['plus_minus']);
 
+//Get the BLAST config from the gene plugin config
+require_once(realpath(__DIR__.'/../../gene/config.php'));
 
-if($plus_minus=="-1"){
-  	$plus_minus="2";  
-   }else{
-	$plus_minus="1"; 
-   }
+$plus_minus = ($strand=="-1")? 'minus':'plus';
+$blastdb_genome_path = $gene_plugin_config['datasets']['genome_blast_dataset_path'];
+$blastdbcmd = $gene_plugin_config['blastdbcmd'];
 
-
-if($plus_minus==2){
+if($plus_minus=='minus'){
 	$ustream_start=$picea_basic_end+1;
 	$ustream_end=$picea_basic_end+$ustream;
 	
-	exec("fastacmd -d  '/mnt/spruce/www/zmarina/data/BLAST/Zmarina_324_genome' -L'".$ustream_start.','.$ustream_end."' -S '".$plus_minus."'  -l 1000000000000000000 -s '".$picea_basic_chromosome."' -D 0;",$output_ustream);
+	exec("$blastdbcmd -db $blastdb_genome_path -range $ustream_start-$ustream_end -strand $plus_minus -entry $picea_basic_chromosome", $output_ustream);
 	
 	
 	$dstream_start=$picea_basic_start-$dstream;
 	$dstream_end=$picea_basic_start-1;
 	
-	exec("fastacmd -d  '/mnt/spruce/www/zmarina/data/BLAST/Zmarina_324_genome' -L'".$dstream_start.','.$dstream_end."' -S '".$plus_minus."'  -l 1000000000000000000 -s '".$picea_basic_chromosome."' -D 0;",$output_dstream);	
+	exec("$blastdbcmd -db  $blastdb_genome_path -range $dstream_start-$dstream_end -strand $plus_minus -entry $picea_basic_chromosome", $output_dstream);	
 	
-	
-	}else{
+	} else{
 		
 	if($picea_basic_start-$ustream>1){
-	$ustream_start=$picea_basic_start-$ustream;
-	$ustream_end=$picea_basic_start-1;	
+        $ustream_start=$picea_basic_start-$ustream;
+        $ustream_end=$picea_basic_start-1;	
 	
-	exec("fastacmd -d  '/mnt/spruce/www/zmarina/data/BLAST/Zmarina_324_genome' -L'".$ustream_start.','.$ustream_end."' -S '".$plus_minus."'  -l 1000000000000000000 -s '".$picea_basic_chromosome."' -D 0;",$output_ustream);
-	}else{
-		$output_ustream[1]="";
-		
-		}
+        exec("$blastdbcmd -db  $blastdb_genome_path -range $ustream_start-$ustream_end -strand $plus_minus -entry $picea_basic_chromosome", $output_ustream);
+	} else{
+		$output_ustream[1]="";	
+    }
 		
 	$dstream_start=$picea_basic_end+1;
 	$dstream_end=$picea_basic_end+$dstream;	
 		
-	exec("fastacmd -d  '/mnt/spruce/www/zmarina/data/BLAST/Zmarina_324_genome' -L'".$dstream_start.','.$dstream_end."' -S '".$plus_minus."'  -l 1000000000000000000 -s '".$picea_basic_chromosome."' -D 0;",$output_dstream);	
+	exec("$blastdbcmd -db  $blastdb_genome_path -range $dstream_start-$dstream_end -strand $plus_minus -entry $picea_basic_chromosome", $output_dstream);	
 	
 	if(strlen($output_ustream[1])>$ustream+1){
-	$output_ustream[1]="";
-}
-if(strlen($output_dstream[1])>$dstream+1){
-	$output_dstream[1]="";
-}	
+        $output_ustream[1]="";
+    }
+    if(strlen($output_dstream[1])>$dstream+1){
+        $output_dstream[1]="";
+    }	
 	
 }
 		
@@ -62,8 +59,6 @@ if($output_ustream[1]==null){
 if($output_dstream[1]==null){
 	$output_dstream[1]="";
 }
-
-
 
 
 $results = array('ustreamstr'=>$output_ustream[1],'dstreamstr'=>$output_dstream[1]);
