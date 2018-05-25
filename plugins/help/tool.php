f2c8a069f25961424d9eb8b8343ddeb48bf5362a
Thomas Van Parys <thpar@psb.ugent.be>
2018-05-25 14:43:56 +0200

    Add Contact item to Help page

--- plugins/help/tool.php
+++ plugins/help/tool.php
@@ -1,15 +1,10 @@
-<?php
-?>
-
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
-  <link rel="stylesheet" type="text/css" href="plugins/help/help/css/demo.css" />
-        <link rel="stylesheet" type="text/css" href="plugins/help/help/css/style.css" />
-		<script type="text/javascript" src="plugins/help/help/js/modernizr.custom.29473.js"></script>
- <link href="https://fonts.googleapis.com/css?family=Bevan" rel="stylesheet" type="text/css">  
+<link rel="stylesheet" type="text/css" href="plugins/help/help/css/demo.css" />
+<link rel="stylesheet" type="text/css" href="plugins/help/help/css/style.css" />
+<script type="text/javascript" src="plugins/help/help/js/modernizr.custom.29473.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Bevan" rel="stylesheet" type="text/css">  
 <link href="https://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet" type="text/css">
-<br />
 <?php  
-
 include_once(realpath(__DIR__.'/Parsedown.php'));
 $Parsedown = new Parsedown();
 
@@ -46,6 +41,12 @@ while ($help_rows_gene_info = mysql_fetch_array($help_results_gene_info)) {
 	$gene_info_string= $help_rows_gene_info['post_content'];
 }
 
+$help_results_contact= mysql_query("select post_content from wp_posts where post_status='publish' AND post_title='Contact';")
+or die(mysql_error()); 
+while ($help_rows_contact = mysql_fetch_array($help_results_contact)) {
+	$contact_string= $help_rows_contact['post_content'];
+}
+
 ?>
 
 <section class="ac-container">
@@ -65,7 +66,15 @@ while ($help_rows_gene_info = mysql_fetch_array($help_results_gene_info)) {
       <p><?php print $Parsedown->text(utf8_encode($exImage_string)) ;?></p>
     </article>
   </div>
-          
+            
+  <div>
+    <input id="ac-16" name="accordion-1" type="checkbox"  />
+    <label for="ac-16">Gene information page</label>
+    <article class="ac-large">
+      <p><?php print $Parsedown->text(utf8_encode($gene_info_string)) ;?></p>
+    </article>
+  </div>
+
   <div>
     <input id="ac-15" name="accordion-1" type="checkbox"  />
     <label for="ac-15">FAQ</label>
@@ -73,12 +82,12 @@ while ($help_rows_gene_info = mysql_fetch_array($help_results_gene_info)) {
       <p><?php print $Parsedown->text(utf8_encode($faq_string)) ;?></p>
     </article>
   </div>
-  
+
   <div>
-    <input id="ac-16" name="accordion-1" type="checkbox"  />
-    <label for="ac-16">Gene information page</label>
+    <input id="ac-17" name="accordion-1" type="checkbox"  />
+    <label for="ac-17">Contact</label>
     <article class="ac-large">
-      <p><?php print $Parsedown->text(utf8_encode($gene_info_string)) ;?></p>
+      <p><?php print $Parsedown->text(utf8_encode($contact_string)) ;?></p>
     </article>
   </div>
   			
