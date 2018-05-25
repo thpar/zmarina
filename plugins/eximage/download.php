2e0889cb3007f721bb186ac67d7a4c4dd9763a50
Thomas Van Parys <thpar@psb.ugent.be>
2018-05-25 16:42:37 +0200

    Tmp download page. PDF and PNG not yet supported.

--- plugins/eximage/download.php
+++ plugins/eximage/download.php
@@ -8,8 +8,18 @@ if ($format == 'svg'){
     header('Content-Type: image/svg+xml');
     header('Content-Disposition: attachment; filename="zostera.svg"');
     print($svg_xml);
-} else {
+} elseif ($format == 'pdf'){
+    header('Content-Type: application/x-pdf');
+    header('Content-Disposition: attachment; filename="zostera.pdf"');
 
+    //$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    //$pdf->AddPage();
+    //$pdf->ImageSVG('@' . $svg_xml, 15, 30, '', '', '', '', '', 1, false);
+    //$pdf->Write(0, $txt='', '', 0, 'L', true, 0, false, false, 0);
+    print($pdf);
+} elseif ($format == 'png'){
+    header('Content-Type: image/png');
+    header('Content-Disposition: attachment; filename="zostera.png"');
 }
 
 
